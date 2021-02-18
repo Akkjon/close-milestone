0bc98b5aecf86c1d38b3456f09b3801c9e5e0450
Akkjon <59337800+Akkjon@users.noreply.github.com>
2021-02-19 00:52:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
+const core = require('@actions/core');
+const { octokit } = require("@octokit/core");
+const milestoneName = core.getInput('milestone_name')
+const isAsync = core.getInput('async')
+let id = null;
 
+async function runAsync() {
+    try {
+        run()
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+function run() {
+    //var owner = process.env.;
+    var repoInfo = process.env.GITHUB_REPOSITORY;
+    var owner = repoInfo.substring(0, repoInfo.indexOf("/"))
+    var repo = repoInfo.substring(repoInfo.indexOf("/") + 1)
+
+    var response = await octokit.request('GET /repos/{owner}/{repo}/milestones', {
+        owner: owner,
+        repo: repo
+      })
+    var jsonResponse = JSON.parse(response);
+    for(var i = 0; i<jsonResponse.length; i++) {
+        var element = jsonResponse[i];
+        if(element.title == milestoneName) {
+            id = element.number;
+            break;
+        }
+    }
+
+    if(id == null) {
+        //ERROR
+    } else {
+        var response = await octokit.request("PATCH /repos/{owner}/{repo}/milestones/{milestone_number}", {
+            owner: owner,
+            repo: repo,
+            milestone_number: id,
+            state: 'closed'
+        })
+    }
+}
+
+if(isAsync) {
+    runAsync()
+} else {
+    run()
+}
