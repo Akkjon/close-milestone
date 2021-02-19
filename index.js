90e0798e378510480ad9e63632653257fc036a43
Akkjon <59337800+Akkjon@users.noreply.github.com>
2021-02-19 10:18:26 +0100

    Updated index.js and added debug (removed later)

--- index.js
+++ index.js
@@ -1,8 +1,77 @@
 const core = require('@actions/core');
 const { octokit } = require("@octokit/core");
+const { createActionAuth } = require("@octokit/auth");
 const milestoneName = core.getInput('milestone_name')
 const isAsync = core.getInput('async')
-let id = null;
+
+const auth = createActionAuth();
+const { token } = await auth();
+console.log("token:")
+console.log(token);
+
+const repoInfo = process.env.GITHUB_REPOSITORY;
+const owner = repoInfo.substring(0, repoInfo.indexOf("/"))
+const repo = repoInfo.substring(repoInfo.indexOf("/") + 1)
+
+console.log("Info:")
+console.log(owner)
+console.log(repo)
+
+
+function getMilestones() {
+    return new Promise((resolve, reject) => {
+        console.log("Fetching milestones...")
+        var response = await octokit.request('GET /repos/{owner}/{repo}/milestones', {
+            headers: {
+                authorization: "Bearer " + token,
+            },
+            owner: owner,
+            repo: repo
+          })
+        if(response.status != 200) {
+            reject();
+            return;
+        }
+        console.log(`Found ${response.data.length} milestones in this repository.`)
+        resolve(response.data);
+    })
+}
+
+function getMilestoneId(milestones) {
+    for(var i = 0; i<milestones.length; i++) {
+        var element = milestones[i];
+        if(element.title == milestoneName) {
+            console.log(`Found milestone ${milestoneName} with id ${element.number}`);
+            return element.number;
+        }
+    }
+    console.warn(`Milestone ${milestoneName} was not found.`)
+    return null;
+}
+
+function deleteMilestone(milestoneNumber) {
+    return new Promise((resolve, reject) => {
+        console.log(`Closing milestone ${milestoneName}(${milestoneNumber})...`)
+        var response = await octokit.request("PATCH /repos/{owner}/{repo}/milestones/{milestone_number}", {
+            headers: {
+                authorization: "Bearer " + token,
+              },
+            owner: owner,
+            repo: repo,
+            milestone_number: id,
+            state: 'closed'
+        })
+        if(response.status != 200) {
+            reject();
+            console.warn(`Milestone ${milestoneName}(${milestoneNumber}) cannot be closed.`)
+            core.setFailed(`Milestone cannot be closed, github responded with an invalid status (${response.status}).`)
+            return;
+        }
+
+        console.log(`Successfully closed milestone ${milestoneName}(${milestoneNumber}).`);
+        resolve();
+    })
+}
 
 async function runAsync() {
     try {
@@ -14,34 +83,15 @@ async function runAsync() {
 }
 
 function run() {
-    //var owner = process.env.;
-    var repoInfo = process.env.GITHUB_REPOSITORY;
-    var owner = repoInfo.substring(0, repoInfo.indexOf("/"))
-    var repo = repoInfo.substring(repoInfo.indexOf("/") + 1)
-
-    var response = await octokit.request('GET /repos/{owner}/{repo}/milestones', {
-        owner: owner,
-        repo: repo
-      })
-    var jsonResponse = JSON.parse(response);
-    for(var i = 0; i<jsonResponse.length; i++) {
-        var element = jsonResponse[i];
-        if(element.title == milestoneName) {
-            id = element.number;
-            break;
-        }
-    }
-
+    let milestones = await getMilestones();
+    let id = getMilestoneId(milestones);
+    console.log("id:")
+    console.log(id)
     if(id == null) {
-        //ERROR
-    } else {
-        var response = await octokit.request("PATCH /repos/{owner}/{repo}/milestones/{milestone_number}", {
-            owner: owner,
-            repo: repo,
-            milestone_number: id,
-            state: 'closed'
-        })
+        core.warning("Action stopped because no milestone was found");
+        return;
     }
+    await deleteMilestone(id)
 }
 
 if(isAsync) {
